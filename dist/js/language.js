b2e2ea783aa0e2bf184f50cedcf7892ba8a23b8d
3a9bf241 <63563095+3a9bf241@users.noreply.github.com>
2021-01-20 22:25:39 +0300

    frinoi

---
+++ dist/js/language.js
@@ -0,0 +1,23 @@
+
+  $(function () {
+        $('#ex1').multilang({
+            defaultLang:'tr',
+            menu: false
+        });
+    });
+
+
+document.getElementById('dropdown02').style.opacity = 0;
+document.getElementById('dropdown03').style.opacity = 0;
+document.getElementById("dropdown02").style.position = "absolute";
+document.getElementById("dropdown02").style.top = "5px";
+document.getElementById("dropdown02").style.left = "12px";
+
+document.getElementById("dropdown02").style.height = "50px";
+document.getElementById("dropdown02").style.width = "50px";
+
+document.getElementById("dropdown03").style.height = "50px";
+document.getElementById("dropdown03").style.width = "50px";
+document.getElementById("dropdown03").style.position = "absolute";
+document.getElementById("dropdown03").style.top = "20px";
+document.getElementById("dropdown03").style.left = "12px";
